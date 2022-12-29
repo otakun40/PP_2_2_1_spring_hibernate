3a19eabec575ede0ac739bb37b7f8664cced8ed2
Bulat Minnigayazov <otakun40@gmail.com>
2022-12-29 23:54:45 +0300

    all done

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,12 +1,11 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -16,11 +15,13 @@ public class UserDaoImp implements UserDao {
    @Autowired
    private SessionFactory sessionFactory;
 
+   @Transactional(rollbackFor = HibernateException.class)
    @Override
    public void add(User user) {
       sessionFactory.getCurrentSession().save(user);
    }
 
+   @Transactional(rollbackFor = HibernateException.class)
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
@@ -28,10 +29,10 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
-   @Transactional(rollbackFor = NoResultException.class)
+   @Transactional(rollbackFor = HibernateException.class)
    @Override
    public List<User> getUserByCar(String model, int series) {
-      return sessionFactory.openSession().createQuery
+      return sessionFactory.getCurrentSession().createQuery
               ("from User user join fetch user.car where user.car.model=:model and user.car.series=:series", User.class)
               .setParameter("model", model)
               .setParameter("series", series)
